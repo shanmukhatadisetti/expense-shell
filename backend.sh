88bc9cfc4a4bdd1ea7e959ca054d0c1d09d58cff
shanmukh <shanmukhatadisetti@gmail.com>
2023-10-19 16:36:43 +0530

    expense mysql

--- backend.sh
+++ backend.sh
@@ -2,6 +2,8 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 dnf install nodejs -y
 
+cp backend.service /etc/systemd/system/backend.service
+
 useradd expense
 
 mkdir /app
@@ -10,7 +12,7 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
-cp backend.service /etc/systemd/system/backend.service
+
 
 systemctl daemon-reload
 
