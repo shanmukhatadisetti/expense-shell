3dedfa518479292ee0e11e91be116ac1d93aa6f8
shanmukh <shanmukhatadisetti@gmail.com>
2023-10-19 16:56:43 +0530

    expense backend

--- backend.sh
+++ backend.sh
@@ -1,22 +1,17 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
 dnf install nodejs -y
 
 cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
-
 mkdir /app
-
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
-
 unzip /tmp/backend.zip
 
 npm install
 
 systemctl daemon-reload
-
 systemctl enable backend
 systemctl start backend
 
