76921c17738ae006a84219af20124f7a2689d770
shanmukh <shanmukhatadisetti@gmail.com>
2023-11-06 20:38:04 +0530

    adding exit status to mysql

--- mysql.sh
+++ mysql.sh
@@ -2,16 +2,21 @@ source common.sh
 
 echo Disable mysql 8 version
 dnf module disable mysql -y &>>$log_file
+echo $?
 
 echo Copy Mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+echo $?
 
 echo install Mysql server
 dnf install mysql-community-server -y &>>$log_file
+echo $?
 
 echo Start mysql service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
+echo $?
 
 echo setup root password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+echo $?
