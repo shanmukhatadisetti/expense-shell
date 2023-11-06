b7316858f15ed80ea098b4e1aecf9af954d3694c
shanmukh <shanmukhatadisetti@gmail.com>
2023-11-06 12:11:24 +0530

    doing nfr to frontend

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
-echo install Nginx
-dnf install nginx -y >>/tmp/expense.log
+echo installing Nginx
+dnf install nginx -y &>>/tmp/expense.log
 
 
 echo copying expense config file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo Removing old nginx content
-rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo Downloading frontend code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 cd /usr/share/nginx/html
 
 echo Extracting frontend code
-unzip /tmp/frontend.zip >>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 echo Starting Nginx services
 systemctl enable nginx >>/tmp/expense.log
