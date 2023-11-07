b57adc6155f0d4bf600408147091774c1aab415e
shanmukh <shanmukhatadisetti@gmail.com>
2023-11-07 17:58:13 +0530

    apply success or failure

--- common.sh
+++ common.sh
@@ -3,9 +3,18 @@ log_file=/tmp/expense.log
 download_and_extract() {
   echo Downloading $component code
   curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-  echo $?
+  stat_check
 
   echo Extracting $component code
   unzip /tmp/$component.zip &>>$log_file
-  echo $?
+  stat_check
+}
+
+stat_check() {
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILED
+    exit 1
+  fi
 }
