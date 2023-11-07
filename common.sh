a4a6374c4a086c3ee26da0b9dc51f3ca321c4707
shanmukh <shanmukhatadisetti@gmail.com>
2023-11-07 19:02:59 +0530

    adding condition to check user add

--- common.sh
+++ common.sh
@@ -2,7 +2,7 @@ log_file=/tmp/expense.log
 
 download_and_extract() {
   echo Downloading $component code
-  curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+  curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
   stat_check
 
   echo Extracting $component code
