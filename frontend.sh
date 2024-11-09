03c1e0897c8d16f8e07a3624fe376528e002e78d
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-09 15:35:04 +0000

    expense shell scripting

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,20 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
-systemctl start nginx
+echo Installing nginx
+dnf install nginx -y >>/tmp/expense.log
 
-systemctl enable nginx
+echo Placing expense config file in nginx
+cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
 
+echo  Removing Old Nginx content
+rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo Download Frontend Code
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 cd /usr/share/nginx/html
 
-unzip /tmp/frontend.zip
+echo Extracting Frontend Code
+unzip /tmp/frontend.zip >>/tmp/expense.log
 
-
-
-systemctl restart nginx
+echo Starting Nginx Service
+systemctl enable nginx >>/tmp/expense.log
+systemctl restart nginx >>/tmp/expense.log
