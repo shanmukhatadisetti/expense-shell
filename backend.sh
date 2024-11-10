ac177f963842ae4f1c830fa7fabc6e385382ea38
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 13:18:55 +0000

    expense shell scripting

--- backend.sh
+++ backend.sh
@@ -1,41 +1,97 @@
-source=common.sh
+source common.sh
 
 echo Disabling nodejs
 dnf module disable nodejs -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
+
 echo Enabling Nodejs 18 version
 dnf module enable nodejs:18 -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo Installing Nodejs
 dnf install nodejs -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
+
 rm -rf /app
 
 echo Adding user
 useradd expense &>>log_file
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 echo Copying Backend service file to system
 cp backend.service /etc/systemd/system/backend.service &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 mkdir /app
 
 echo Downloading backend content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 cd /app
 
 echo Extracting Backend Content
 unzip /tmp/backend.zip &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo Installing Nodejs Dependencies
 npm install &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo starting backend services
 systemctl daemon-reload &>>log_file
 
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo Installing Mysql Client
 dnf install mysql -y
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo Loading Schema
 mysql -h mysql.autonagar.in -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
