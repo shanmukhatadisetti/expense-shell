2e2e86672c960c0efff56e9456cc7edfa1b0f554
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 13:36:20 +0000

    expense shell scripting

--- backend.sh
+++ backend.sh
@@ -1,108 +1,48 @@
 source common.sh
+component=backend
 
 echo Disabling nodejs
 dnf module disable nodejs -y &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Enabling Nodejs 18 version
 dnf module enable nodejs:18 -y &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Installing Nodejs
 dnf install nodejs -y &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 rm -rf /app
 
 echo Adding user
 useradd expense &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 echo Copying Backend service file to system
 cp backend.service /etc/systemd/system/backend.service &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 mkdir /app
 
-echo Downloading backend content
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
-
 cd /app
 
-echo Extracting Backend Content
-unzip /tmp/backend.zip &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+download_and_extract
 
 echo Installing Nodejs Dependencies
 npm install &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 echo starting backend services
 systemctl daemon-reload &>>log_file
 
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Installing Mysql Client
 dnf install mysql -y
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Loading Schema
 mysql -h mysql.autonagar.in -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+stat_check
