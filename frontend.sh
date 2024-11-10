984113035fe55620db386e3be907902ae716366b
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 12:47:12 +0000

    expense shell scripting

--- frontend.sh
+++ frontend.sh
@@ -2,21 +2,56 @@ source common.sh
 
 echo Installing nginx
 dnf install nginx -y &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo Placing expense config file in nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo  Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo Download Frontend Code
 curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 cd /usr/share/nginx/html
 
 echo Extracting Frontend Code
 unzip /tmp/frontend.zip &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo Starting Nginx Service
 systemctl enable nginx &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 systemctl restart nginx &>>log_file
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
