afc6665f72b5b7ee56af6ca2c933c9092401c6d0
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 12:55:47 +0000

    expense shell scripting

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ source common.sh
 echo Installing nginx
 dnf install nginx -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS/e[0m"
 else
   echo FAILURE
 fi
@@ -44,11 +44,7 @@ fi
 
 echo Starting Nginx Service
 systemctl enable nginx &>>log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+
 systemctl restart nginx &>>log_file
 if [ $? -eq 0 ]; then
   echo SUCCESS
