c9542be3f14c025a616cd7298d163d489db44e05
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 15:06:50 +0000

    expense shell scripting

--- mysql.sh
+++ mysql.sh
@@ -2,16 +2,21 @@ source common.sh
 
 echo Disabling Mysql
 dnf module disable mysql -y &>>$log_file
+stat_check
 
 echo Copying Mysql REpo
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+stat_check
 
 echo Installing Mysql
 dnf install mysql-community-server -y &>>$log_file
+stat_check
 
 echo Mysql Services
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
+stat_check
 
 echo Setting up Mysql Username and Password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+stat_check
