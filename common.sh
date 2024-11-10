7c42310faf9e94a3266eff43df41d814947298a9
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2024-11-10 13:33:55 +0000

    expense shell scripting

--- common.sh
+++ common.sh
@@ -1,1 +1,22 @@
 log_file=/tmp/expense.log
+
+download_and_extract() {
+
+  echo Download $component Code
+  curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>log_file
+  stat_check
+
+  echo Extracting $component Code
+  unzip /tmp/$component.zip &>>log_file
+  stat_check
+}
+
+stat_check() {
+
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -1,53 +1,24 @@
 source common.sh
-
+component=frontend
 echo Installing nginx
 dnf install nginx -y &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+stat_check
 
 echo Placing expense config file in nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[om"
-fi
+stat_check
 
 echo  Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
-
-echo Download Frontend Code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+stat_check
 
 cd /usr/share/nginx/html
 
-echo Extracting Frontend Code
-unzip /tmp/frontend.zip &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+
+download_and_extract
 
 echo Starting Nginx Service
 systemctl enable nginx &>>log_file
 
 systemctl restart nginx &>>log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+stat_check
