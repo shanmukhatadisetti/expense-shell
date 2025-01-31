e9e3c30dd472b6b1c329c83ad0b7d7de6c2eaf9f
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2025-01-31 16:59:14 +0000

    shell script

--- backend.sh
+++ backend.sh
@@ -4,7 +4,7 @@ cp backend.service /etc/systemd/system/backend.service
 dnf install nodejs -y
 
 useradd expense
-
+rm -rf /app
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
