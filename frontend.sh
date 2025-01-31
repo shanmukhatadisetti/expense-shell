fbfc76ef291a939b753985d5ad49bcdf3b46980f
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2025-01-31 18:33:25 +0000

    shell script

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,17 @@
 source common.sh
+logs=/tmp/expense.log
 component=frontend
 
 echo Installing Nginx
-dnf install nginx -y
+dnf install nginx -y &>>$logs
 check_status
 
 echo Placing expense conf file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$logs
 check_status
 
 echo Removing Old Content
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$logs
 check_status
 
 cd /usr/share/nginx/html
@@ -19,6 +20,6 @@ cd /usr/share/nginx/html
 Download_and_Extarct
 
 echo Starting Nginx Service
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>$logs
+systemctl restart nginx &>>$logs
 check_status
--- mysql.sh
+++ mysql.sh
@@ -1,20 +1,21 @@
+logs=/tmp/expense.log
 echo Disabling mysql module
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$logs
 check_status
 
 echo Copying mysql repo file
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$logs
 check_status
 
 echo Installing Mysql
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>$logs
 check_status
 
 echo Enabling Mysql Services
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>$logs
+systemctl start mysqld &>>$logs
 check_status
 
 echo Setting-up Mysql-Root-Password
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logs
 check_status
