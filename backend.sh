041499c218c1b567ba0dc8924b5b4ed66ebd3fac
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2025-01-31 17:10:29 +0000

    shell script

--- backend.sh
+++ backend.sh
@@ -1,23 +1,23 @@
 logs=/tmp/expense.log
-dnf module disable nodejs -y &>>logs
-dnf module enable nodejs:18 -y &>>logs
+dnf module disable nodejs -y &>>$logs
+dnf module enable nodejs:18 -y &>>$logs
 cp backend.service /etc/systemd/system/backend.service
-dnf install nodejs -y &>>logs
+dnf install nodejs -y &>>$logs
 
-useradd expense &>>logs
+useradd expense &>>$logs
 rm -rf /app
-mkdir /app &>>logs
+mkdir /app &>>$logs
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>logs
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$logs
 cd /app
-unzip /tmp/backend.zip &>>logs
+unzip /tmp/backend.zip &>>$logs
 
-npm install &>>logs
-systemctl daemon-reload &>>logs
+npm install &>>$logs
+systemctl daemon-reload &>>$logs
 
-systemctl enable backend &>>logs
-systemctl start backend &>>logs
+systemctl enable backend &>>$logs
+systemctl start backend &>>$logs
 
-dnf install mysql -y &>>logs
+dnf install mysql -y &>>$logs
 
-mysql -h mysql.autonagar.in -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>logs
+mysql -h mysql.autonagar.in -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$logs
